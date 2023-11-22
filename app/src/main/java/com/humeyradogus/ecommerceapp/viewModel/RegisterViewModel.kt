7442c4687bef07c32aeac1ffb0850c8c16a484f6
humeyradogus <75491382+humeyradogus@users.noreply.github.com>
2023-11-23 00:24:14 +0300

    Firestore connection added!

--- app/src/main/java/com/humeyradogus/ecommerceapp/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/humeyradogus/ecommerceapp/viewModel/RegisterViewModel.kt
@@ -3,6 +3,7 @@ package com.humeyradogus.ecommerceapp.viewModel
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.humeyradogus.ecommerceapp.data.User
 import com.humeyradogus.ecommerceapp.util.*
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,10 +15,11 @@ import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
-class RegisterViewModel @Inject constructor(private val firebaseAuth: FirebaseAuth) : ViewModel() {
+class RegisterViewModel @Inject constructor(private val firebaseAuth: FirebaseAuth,
+                                            private val db:FirebaseFirestore) : ViewModel() {
 
-    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Unspecified())
-    val register: Flow<Resource<FirebaseUser>> = _register
+    private val _register = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val register: Flow<Resource<User>> = _register
 
     private val _validation = Channel<RegisterFieldsState>()
     val validation = _validation.receiveAsFlow()
@@ -30,7 +32,8 @@ class RegisterViewModel @Inject constructor(private val firebaseAuth: FirebaseAu
             firebaseAuth.createUserWithEmailAndPassword(user.email, password)
                 .addOnSuccessListener {
                     it.user?.let {
-                        _register.value = Resource.Success(it)
+                        //_register.value = Resource.Success(it)
+                        saveUserInfo(it.uid,user)
                     }
 
                 }.addOnFailureListener {
@@ -43,6 +46,18 @@ class RegisterViewModel @Inject constructor(private val firebaseAuth: FirebaseAu
                 _validation.send(registerFieldsState)
             }
         }
+    }
+
+    private fun saveUserInfo(userUid: String, user:User) {
+        db.collection("user")
+            .document(userUid)
+            .set(user)
+            .addOnSuccessListener {
+                _register.value = Resource.Success(user)
+            }
+            .addOnFailureListener {
+                _register.value = Resource.Error(it.message.toString())
+            }
     }
 
     private fun checkValidation(user: User, password: String): Boolean {
