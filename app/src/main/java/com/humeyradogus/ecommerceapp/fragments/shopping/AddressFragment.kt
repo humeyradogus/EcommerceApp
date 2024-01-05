aa93d1e53732a6a21ade95252c65a8c7b2447b6c
humeyradogus <75491382+humeyradogus@users.noreply.github.com>
2024-01-05 22:23:28 +0300

    User can save the address

---
+++ app/src/main/java/com/humeyradogus/ecommerceapp/fragments/shopping/AddressFragment.kt
@@ -0,0 +1,84 @@
+package com.humeyradogus.ecommerceapp.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.humeyradogus.ecommerceapp.R
+import com.humeyradogus.ecommerceapp.data.Address
+import com.humeyradogus.ecommerceapp.databinding.FragmentAddressBinding
+import com.humeyradogus.ecommerceapp.databinding.FragmentCartBinding
+import com.humeyradogus.ecommerceapp.util.Resource
+import com.humeyradogus.ecommerceapp.viewModel.AddressViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class AddressFragment : Fragment() {
+    private lateinit var binding: FragmentAddressBinding
+    val viewModel by viewModels<AddressViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addNewAddress.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarAddress.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressbarAddress.visibility = View.INVISIBLE
+                        findNavController().navigateUp()
+
+                    }
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(),it.message, Toast.LENGTH_LONG).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.error.collectLatest {
+                Toast.makeText(requireContext(),it, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddressBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonSave.setOnClickListener {
+                val addressTitle = edAddressTitle.text.toString()
+                val fullname = edFullName.text.toString()
+                val street = edStreet.text.toString()
+                val phone = edPhone.text.toString()
+                val city = edCity.text.toString()
+                val state = edState.text.toString()
+                val address = Address(addressTitle,fullname,street,phone, city, state)
+
+
+                viewModel.addAddress(address)
+            }
+        }
+
+    }
+}
