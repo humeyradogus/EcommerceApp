464eb234c51f379d7c7b58a3b572e58e8e7aa329
humeyradogus <75491382+humeyradogus@users.noreply.github.com>
2024-01-15 00:36:17 +0300

    Order status can be viewed!

--- app/src/main/java/com/humeyradogus/ecommerceapp/data/Order.kt
+++ app/src/main/java/com/humeyradogus/ecommerceapp/data/Order.kt
@@ -2,11 +2,16 @@ package com.humeyradogus.ecommerceapp.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.text.SimpleDateFormat
 import java.util.*
-
+import kotlin.random.Random.Default.nextLong
 
 data class Order(
-    val orderStatus: String,
-    val totalPrice:Float,
-    val products:List<CartProduct>,
-    val address:Address)
+    val orderStatus: String = "",
+    val totalPrice:Float = 0f,
+    val products:List<CartProduct> = emptyList(),
+    val address:Address = Address(),
+    val date: String = SimpleDateFormat("yyyyy-MM-dd", Locale.ENGLISH).format(Date()),
+    val orderId: Long = nextLong(0,100_000_000_000) + totalPrice.toLong()
+)
+
